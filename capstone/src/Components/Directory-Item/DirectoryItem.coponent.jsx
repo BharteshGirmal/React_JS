a146d2c8c95c5b92895c86120a5ca925730f8144
BharteshGirmal <bharteshgirmal@gmail.com>
2024-08-26 20:09:46 +0530

    adding-redirect

--- capstone/src/Components/Directory-Item/DirectoryItem.coponent.jsx
+++ capstone/src/Components/Directory-Item/DirectoryItem.coponent.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import './DirectoryItem.styles.scss'
+import { useNavigate } from 'react-router-dom';
 export default function CategoryItem ({category}) {
 
-const { imageUrl, title}= category;
+const { imageUrl, title, route}= category;
+const navigate= useNavigate();
+const handleNavigate = ()=>{
+  navigate(route);
+}
 console.log(category);
 console.log(title);
   return ( 
       <div  className="directory-container">
       <div className='background-image' style={{backgroundImage:`url(${imageUrl})`}} />
-      <div className="directory-body-container">
+      <div className="directory-body-container" onClick={handleNavigate}>
       <h1>{title}</h1>
       <p>Shop Now</p>
       </div>
